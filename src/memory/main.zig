6c3daf28030b5c444f25696f1d3ec1e6fbc21894
Serhii Ostrikov <serhii.ostrikov@cpcs.ws>
2025-06-15 13:21:38 +0300

    smp allocator

--- src/memory/main.zig
+++ src/memory/main.zig
@@ -4,10 +4,12 @@ pub fn main() !void {
     // std.mem.Allocator is a generic object for allocating memory
     // It contains a generic pointer (*anyopaque) and a vtable pointer (*Vtable)
 
+    // basic allocator that would get an entire page for each allocation
     const allocator = std.heap.page_allocator;
 
     // memory is a slice
     const memory = try allocator.alloc(u32, 5);
+    defer allocator.free(memory);
 
     std.debug.print(
         "Size of memory is {d}, type is {s}\n",
@@ -17,9 +19,25 @@ pub fn main() !void {
         },
     );
 
-    @memset(memory, 0);
+    for (0..memory.len) |i| {
+        // intCat since @sizeOf(usize) >= @sizeOf(u32)
+        memory[i] = @intCast(i + 1);
+    }
 
     for (memory, 0..) |value, i| {
         std.debug.print("[{d}] = {d}\n", .{ i, value });
     }
+
+    // now since GPA is dead we use SMP allocator
+    const smp = std.heap.smp_allocator;
+
+    const parts = [_][]const u8{ "Hello", ", ", "world!" };
+    const concatted = std.mem.concat(
+        smp,
+        u8,
+        &parts,
+    ) catch @panic("OOM");
+    defer smp.free(concatted);
+
+    std.debug.print("Concatted string is {s}\n", .{concatted});
 }
